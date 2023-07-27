4a2ae1e85e8a1ed170b0e418b0180733eb720239
Artuom4ik <masterskayaigr123@gmail.com>
2023-07-27 20:28:22 +0300

    show flats owners.

--- property/admin.py
+++ property/admin.py
@@ -3,8 +3,16 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class OwnersInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ('owner',)
+
+
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
+    inlines = [
+        OwnersInline,
+    ]
     search_fields = ('town','address',)
     readonly_fields = ['created_at',]
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
